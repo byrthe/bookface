83456d2308cdc5e8c434e7000efdbe14c1795fb2
Byrthe Laptop <byrthelemmens@gmail.com>
2021-07-07 13:56:49 +0200

    task manager is connected and working again, on the todo.ejs page

--- index.js
+++ index.js
@@ -61,53 +61,53 @@ app.use('/users',require('./routes/users'));
 
 // to do list methods underneath >>>
 // GET
-// app.get("/", (req, res) => {
-//   TodoTask.find({}, (err, tasks) => {
-//     console.log('hello');
-//   //   console.log(tasks);
-//   // res.render("todo.ejs", { todoTasks: tasks });
-//   });
-//   });
+app.get("/todo", (req, res) => {
+  TodoTask.find({}, (err, tasks) => {
+    console.log('hello');
+    //   console.log(tasks);
+    res.render("todo.ejs", { todoTasks: tasks });
+    });
+  });
 
-// //POST 
-// app.post('/',async (req, res) => {
-//   const todoTask = new TodoTask({
-//   content: req.body.content
-//   });
-//   try {
-//   await todoTask.save();
-//   res.redirect("/");
-//   } catch (err) {
-//   res.redirect("/");
-//   }
-//   });
+//POST 
+app.post('/',async (req, res) => {
+  const todoTask = new TodoTask({
+    content: req.body.content
+  });
+  try {
+    await todoTask.save();
+    res.redirect("/todo");
+  } catch (err) {
+    res.redirect("/todo");
+  }
+});
 
 
-//   //UPDATE
-// app
-// .route("/edit/:id")
-// .get((req, res) => {
-// const id = req.params.id;
-// TodoTask.find({}, (err, tasks) => {
-// res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-// });
-// })
-// .post((req, res) => {
-// const id = req.params.id;
-// TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-// if (err) return res.send(500, err);
-// res.redirect("/");
-// });
-// });
+  //UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+  const id = req.params.id;
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+  });
+})
+.post((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/todo");
+  });
+});
 
-// //DELETE
-// app.route("/remove/:id").get((req, res) => {
-//   const id = req.params.id;
-//   TodoTask.findByIdAndRemove(id, err => {
-//   if (err) return res.send(500, err);
-//   res.redirect("/");
-//   });
-//   });
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, err => {
+  if (err) return res.send(500, err);
+  res.redirect("/todo");
+  });
+  });
 
 
 
