1fd2580eeb4df6791c4b27888fe8b2f148128080
avandewauwer <annesophievandewauwer@gmail.com>
2021-07-13 09:52:14 +0200

    css

--- public/scripts/floor.js
+++ public/scripts/floor.js
@@ -1,13 +1,12 @@
 // FRONT END FILE TO INTERACT WITH THE DOM
-const searchInput = document.querySelector('#searchInput');
-const searchButton = document.querySelector('#searchButton');
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
 const searchTerm = searchInput.value;
 
 const area = document.querySelector(".profile-cover__info");
 
-searchButton.addEventListener('click', (e) => {
-    console.log('clicked me!');
-    console.log(searchInput.value);
-    area.insertAdjacentHTML('beforeend', searchTerm);
+searchButton.addEventListener("click", (e) => {
+  console.log("clicked me!");
+  console.log(searchInput.value);
+  area.insertAdjacentHTML("beforeend", searchTerm);
 });
-
